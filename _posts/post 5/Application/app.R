63d70bb15eaa6e65e46c2f684e3f66af37cb5178
vvuu0009 <vvuu0009@student.monash.edu>
2020-11-26 00:07:29 +1100

    fixing application code

--- _posts/post 5/Application/app.R
+++ _posts/post 5/Application/app.R
@@ -6,13 +6,14 @@ library(broom)
 library(patchwork)
 library(shiny)
 library(shinydashboard)
+library(gridExtra)
 
 # reading data 
 
-height_country <- read.csv("data/NCD_RisC_eLife_2016_height_age18_countries.txt")
-BP_country <- read.csv("data/NCD_RisC_Lancet_2016_BP_age_standardised_countries.txt")
-BMI_country <- read.csv("data/NCD_RisC_Lancet_2017_BMI_age_standardised_country.txt")
-colesteral_country <- read.csv("data/NCD_RisC_Nature_2020_Cholesterol_age_standardised_countries.txt")
+height_country <- read.csv("data/NCD_RisC_eLife_2016_height_age18_countries.csv")
+BP_country <- read.csv("data/NCD_RisC_Lancet_2016_BP_age_standardised_countries.csv")
+BMI_country <- read.csv("data/NCD_RisC_Lancet_2017_BMI_age_standardised_country.csv")
+colesteral_country <- read.csv("data/NCD_RisC_Nature_2020_Cholesterol_age_standardised_countries.csv")
 country_status <- read.csv("data/country.csv") %>%
     mutate(Status = as.factor(Status))
 
@@ -22,30 +23,30 @@ height <- height_country %>%
     mutate(continent = countrycode(sourcevar = Country,
                                    origin = "country.name",
                                    destination = "continent")) %>%
-    select(Country,continent,ISO,Sex,Year.of.birth,Mean.height..cm.) %>%
+    select(Country,continent,Sex,Year.of.birth,Mean.height..cm.) %>%
     rename(height = Mean.height..cm.)
 
 BP <- BP_country %>%
-    select(Country.Region.World,ISO,Sex,Year,Mean.diastolic.blood.pressure..mmHg.,Mean.systolic.blood.pressure..mmHg.,Prevalence.of.raised.blood.pressure)
+    select(Country.Region.World,Sex,Year,Mean.diastolic.blood.pressure..mmHg.,Mean.systolic.blood.pressure..mmHg.,Prevalence.of.raised.blood.pressure)
 
 BP_ID <- BP %>%
     mutate(ID = paste(Country.Region.World,Sex,Year)) %>%
-    select(-Country.Region.World,-ISO,-Sex,-Year)
+    select(-Country.Region.World,-Sex,-Year)
 
 
 BMI <- BMI_country %>%
-    select(Country.Region.World,ISO,Sex,Year,Mean.BMI)
+    select(Country.Region.World,Sex,Year,Mean.BMI)
 
 BMI_ID <- BMI %>%
     mutate(ID = paste(Country.Region.World,Sex,Year))%>%
-    select(-Country.Region.World,-ISO,-Sex,-Year)
+    select(-Country.Region.World,-Sex,-Year)
 
 Col <- colesteral_country %>%
-    select(Country.Region.World,ISO,Sex,Year,Mean.total.cholesterol..mmol.L.,Mean.non.HDL.cholesterol..mmol.L.,Mean.HDL.cholesterol..mmol.L.)
+    select(Country.Region.World,Sex,Year,Mean.non.HDL.cholesterol..mmol.L.)
 
 Col_ID <- Col %>%
     mutate(ID = paste(Country.Region.World,Sex,Year))%>%
-    select(-Country.Region.World,-ISO,-Sex,-Year)
+    select(-Country.Region.World,-Sex,-Year)
 
 height_join <- height %>%
     mutate(ID = paste(Country,Sex,Year.of.birth))
@@ -257,92 +258,18 @@ country_plot <- country_status %>%
                                    origin = "country.name",
                                    destination = "continent"))
 
-## Reference 
-
-reftab <- tibble(References = c("Arel-Bundock, Vincent, Nils Enevoldsen, and CJ Yetman. 2018. “Countrycode: An R Package to Convert Country Names and Country Codes.” Journal of Open Source Software 3 (28): 848. https://doi.org/10.21105/joss.00848.",
-                                "Auguie, Baptiste. 2017. GridExtra: Miscellaneous Functions for 'Grid' Graphics. https://CRAN.R-project.org/package=gridExtra.",
-                                "“Body Mass Index - Bmi.” 2020. World Health Organization. World Health Organization. https://www.euro.who.int/en/health-topics/disease-prevention/nutrition/a-healthy-lifestyle/body-mass-index-bmi.",
-                                "Health & Human Services, Department of. 2014. “Cholesterol.” Better Health Channel. Department of Health & Human Services. https://www.betterhealth.vic.gov.au/health/conditionsandtreatments/cholesterol.",
-                                "“High Cholesterol Is Responsible for About 3.9 Million Worldwide Deaths.” n.d. RisC. http://ncdrisc.org/.",
-                                "“List of Developing Countries as Declared by the Minister for Foreign Affairs.” n.d. DFAT. https://www.dfat.gov.au/about-us/publications/Pages/list-of-developing-countries-as-declared-by-the-minister-for-foreign-affairs.",
-                                "“Physical Inactivity: A Global Public Health Problem.” 2014. World Health Organization. World Health Organization. https://www.who.int/dietphysicalactivity/factsheet_inactivity/en/.",
-                                "“Raised Blood Pressure.” 2015. World Health Organization. World Health Organization. https://www.who.int/gho/ncd/risk_factors/blood_pressure_prevalence_text/en/#:~:text=Globally, the overall prevalence of,modestly between 1980 and 2008.",
-                                "Robinson, David, Alex Hayes, and Simon Couch. 2020. Broom: Convert Statistical Objects into Tidy Tibbles. https://CRAN.R-project.org/package=broom",
-                                "Tierney, Nicholas. 2017. “Visdat: Visualising Whole Data Frames.” JOSS 2 (16): 355. https://doi.org/10.21105/joss.00355.",
-                                "Wickham, Hadley, Mara Averick, Jennifer Bryan, Winston Chang, Lucy D’Agostino McGowan, Romain François, Garrett Grolemund, et al. 2019. “Welcome to the tidyverse.” Journal of Open Source Software 4 (43): 1686. https://doi.org/10.21105/joss.01686",
-                                "Zhu, Hao. 2020. KableExtra: Construct Complex Table with ’Kable’ and Pipe Syntax. https://CRAN.R-project.org/package=kableExtr"))
-
-
 
 ui <- dashboardPage(
     dashboardHeader(title = "FIT5147: Narrative Visualisation Project Project"),
     dashboardSidebar(
         sidebarMenu(
-            menuItem("Title", tabName = "dashboard1"),
-            menuItem("Introduction", tabName = "dashboard2"),
-            menuSubItem("Data Wrangling", tabName = "dashboard3"),
-            menuSubItem("Data Checking", tabName = "dashboard4"),
             menuItem("World and Country Trends", tabName = "dashboard5"),
             menuItem("Health Factor Regression Analysis", tabName = "dashboard6"),
-            menuItem("Continent and Wealth Level Trends", tabName = "dashboard7"),
-            menuItem("Conclusion", tabName = "dashboard8"),
-            menuSubItem("Reflection", tabName = "dashboard9"),
-            menuItem("References", tabName = "dashboard10")
+            menuItem("Continent and Wealth Level Trends", tabName = "dashboard7")
             )
         ),
     dashboardBody(
         tabItems(
-            tabItem(tabName = "dashboard1",
-                    fluidPage(
-                        titlePanel("FIT5147 Narrative Visualisation Project: Exploring World Health Data"),
-                        h3("Vinny Vu ")
-                    )
-            ),
-            tabItem(tabName = "dashboard2",
-                    fluidPage(
-                        titlePanel("Introduction"),
-                        textOutput("intro1"),
-                        br(),
-                        textOutput("intro2"),
-                        br(),
-                        textOutput("intro3"),
-                        br(),
-                        textOutput("intro4"),
-                        br(),
-                        textOutput("intro5"),
-                        br(),
-                        textOutput("intro6"),
-                        h4("•	What is the trend in the world health data across time and what are the specific trends for each country (BMI, Height, Blood Pressure and Cholesterol)?"),
-                        h4("•	Is there a relationship between BMI and other health factors (Height, Blood Pressure and Cholesterol) and what is it?"),
-                        h4("•	Do countries within the same continent or with similar cultures share similar results and trends and what are these results/trends?")
-                    )
-            ),
-            tabItem(tabName = "dashboard3",
-                    fluidPage(
-                        titlePanel("Data Wrangling"),
-                        textOutput("Wran1"),
-                        br(),
-                        textOutput("Wran2"),
-                        br(),
-                        textOutput("Wran3"),
-                        br(),
-                        textOutput("Wran4"),
-                        br(),
-                        textOutput("Wran5"),
-                        br(),
-                        textOutput("Wran6"),
-                        br(),
-                        textOutput("Wran7")
-                    )
-            ),
-            tabItem(tabName = "dashboard4",
-                    fluidPage(
-                        titlePanel("Data Checking"),
-                        textOutput("Check1"),
-                        br(),
-                        textOutput("Check2")
-                    )
-            ),
             tabItem(tabName = "dashboard5",
                     fluidPage(
                         titlePanel("What is the trend in the world health data across time and what are the specific trends for each country (BMI, Height, Blood Pressure and Cholesterol)?"),
@@ -396,25 +323,7 @@ ui <- dashboardPage(
                                 )
                             )
                         )
-                    ),
-            tabItem(tabName = "dashboard8",
-                    fluidPage(
-                        titlePanel("Conclusion"),
-                        textOutput("Conc")
                     )
-            ),
-            tabItem(tabName = "dashboard9",
-                    fluidPage(
-                        titlePanel("Reflection"),
-                        textOutput("reflect")
-                    )
-            ),
-            tabItem(tabName = "dashboard10",
-                    fluidPage(
-                        titlePanel("References"),
-                        tableOutput("reftab")
-                    )
-            )
             )
         )
     )
@@ -743,29 +652,6 @@ server <- function(input, output) {
                 theme_minimal()
         }
     })
-    
-    output$intro1 <- renderText("In today’s it is common belief that we are becoming increasingly unhealthy because of the move in diets towards often convenient unhealthy food and increased inactivity (World Health Organisation, 2014). To assess this statement, we will be looking into the following health factors, height, body mass index, blood pressure and cholesterol and their changes across time.")
-    output$intro2 <- renderText("It is common belief that due to the better access to quality food, nutrients and wealth humans are growing taller over time and therefore it is of interest to analysis the changes in height data.")
-    output$intro3 <- renderText("Per the World Health Organization (World Health Organisation,2015) Hypertension or raised blood pressure is the condition in which the blood vessels have persistently raised pressure and the higher the pressure, the harder the heart must pump. Hypertension is a serious medical condition and can increase the risk of heart, brain, kidney, and other diseases being one of the major causes of premature deaths worldwide (World Health Organisation, 2015.) Therefore, it is of interest to assess the trends of raised blood pressure data.")
-    output$intro4 <- renderText("Body mass index or BMI for short, is a measure for indicating nutritional status in adults defined as a person’s weight in kilograms divided by the square of the person’s height in meters (kg/m2) (World Health Organisation, 2020). BMI was developed as a risk indicator of diseases, the higher the BMI is often resulted in a higher level of excessive body fat resulting the increased risk of some diseases such as premature death, cardiovascular diseases, blood pressure, osteoarthritis, some cancers and diabetes (World Health Organisation, 2020). Therefore, it is of interest to assess the trends in BMI levels.")
-    output$intro5 <- renderText("Per betterhealth Health & Human Services (2014) Cholesterol is defined as a type of fat that is part of all animal cells that is essential for many of the body’s metabolic processes, including the production of hormones, bile and vitamin D. Specifically Low-density lipoprotein (LDL) cholesterol, carries most of the cholesterol that is delivered to cells and is often referred to as “bad” cholesterol as when its level in the bloodstream is high, it can clog up your arteries . High cholesterol leads to fatty deposits developing in the arteries which causes the vessel to narrow eventually becoming blocked leading to heart disease and stroke (BHC, 2014.) Therefore, it is of interest to assess the trends in LDL cholesterol levels.")
-    output$intro6 <- renderText("Specifically, we will be analysing world health data to answer the following questions:")
-    output$Wran1 <- renderText("Data for the analysis has been taken from the NCD Risk Factor Collaboration (NCD-RisC) a network of health scientist around the world that provides rigorous and timely data on risk factors for non-communicable diseases (NCDs) for 200 countries and territories (RisC, n.d.).")
-    output$Wran2 <- renderText("Body-Mas Index data: This data set measures the yearly average national adult body-mass index for all countries from 1975 to 2016. The variables include the country, sex, year, mean BMI with the lower and upper 95% uncertainty intervals, and the prevalence of individuals falling into each BMI level with the lower and upper 95% uncertainty intervals. For the purposes of analysis, we will only be looking at the mean BMI for each country.")
-    output$Wran3 <- renderText("The data sets used include the following:")
-    output$Wran4 <- renderText("Height data: This data set measures the yearly mean height for adults (age 18) for all countries from 1896 to 1996. The variables include the country, sex, year of birth, mean height in cm with the lower and upper 95% uncertainty intervals.")
-    output$Wran5 <- renderText("Blood Pressure data: This data set measures the yearly mean blood pressure readings (systolic, diastolic and prevalence of raised blood pressure) for adults for all countries from 1975 to 2015. The variables include the country, sex, year, mean systolic blood pressure reading (in mmHg) with the upper and lower 95% uncertainty interval, mean diastolic blood pressure reading (in mmHg) with the upper and lower 95% uncertainty interval and the prevalence of raised blood pressure with the upper and lower 95% uncertainty interval. For the purposes of analysis, we will only be looking at the prevalence of raised blook pressure for each country. ")
-    output$Wran6 <- renderText("Cholesterol data: This data set measures the yearly mean cholesterol readings (total cholesterol, non-HDL cholesterol, HDL cholesterol) for adults for all countries from 1980 to 2018. The variables include the country, sex, year, mean total cholesterol reading (in mmol.L) with the upper and lower 95% uncertainty interval, mean non HDL cholesterol reading (in mmol.L) with the upper and lower 95% uncertainty interval and the mean HDL cholesterol reading (in mmol.L) with the upper and lower 95% uncertainty interval. For the purposes of analysis, we will only be looking at the mean non-HDL cholesterol readings for each country.")
-    output$Wran7 <- renderText("For the analysis of question 3 we are interested in analysing reading across each continent and developed/ developing countries. To obtain which countries fall into each continent the r package countrycode (Arel-Bundock, Enevoldsen, and Yetman, 2018) was used and to obtain which countries fall into the developed/ developing categories the list was obtained from the Australian Government Department of Foreign Affairs and Trade website (DFAT, n.d.). The developed/developing countries list was converted into excel form to allow importing into R. A left join was used to add the continent and wealth status to each data set. To compare the relationship between BMI with height, blood pressure and cholesterol all data sets were joined using an inner join.")
-    output$Check1 <- renderText("The visdat R package was the main tool used in data checking (Tierney, 2017). After checking the NCD-RisC data for completeness it was found there was no missing data. As all the health data was compiled by NCD-RisC naming conventions of countries, country codes and years were all the same and therefore no modifications were needed to variables to allow joining data sets. However, the joined data used for the analysis in section 2 was reduced between the years of 1980-1996 being the only common years across all data sets.")
-    output$Check2 <- renderText("Adding the continents with the countrycode package (Arel-Bundock, Enevoldsen, and Yetman, 2018) was able to be used without any missing values and therefore, manipulation of the input countries was not needed. However, naming conventions of countries between differ between the NDC-RisC data and the Developing/Developed country list obtained from Australian Government Department of Foreign Affairs and Trade website. The Developing/Developed country excel list was therefore modified to ensure same naming conventions between both data sets.")
-    output$Conc <- renderText("From the analysis conducted we were able to analyse the world health data to explore the trends in country and world height, BMI, blood pressure and cholesterol. In section 4.1 we can see there is an overall upward trend in world height and BMI but an overall downward trend in high blood pressure and cholesterol. However, due to the large amount of countries analysed we were unable to assess individual country trends but use stacked line graphs to assess the overall trend and differences between countries. From section 4.2 we can see there is not a strong relationship between BMI and height or blood pressure, however, there appears to be a strong positive relationship between BMI and Cholesterol. In section 4.3 we were able to analyse the trends in health data between different continents and wealth levels. From the analysis we can see that countries with lower wealth levels appear to record better health readings having lower BMI, blood pressure and Cholesterol readings. However, over time lower wealth countries experience worsening reading whereas, higher wealth countries experience improvements in averages.")
-    output$reflect <- renderText("From this analysis I was able to use to use Wickham et al. (2019), Auguie (2017), Arel-Bundock, Enevoldsen, and Yetman (2018), Zhu (2020) and Robinson, Hayes, and Couch (2020) to analyse the world health data using various line graphs, dot plots, regression lines and analyses using ordinary least squares. To further improve this report however, specific countries could have been chosen for analysis as comparing 200 countries on one graph did not reveal much information. Further, other models could be used to assess the relationship between BMI and other health factors could be used given the weak outputs obtained.")
-    
-    output$reftab <- renderTable(
-        reftab
-    )
-
 }
 
  
